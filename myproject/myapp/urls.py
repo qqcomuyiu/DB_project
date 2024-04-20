6fef1d4edb0e4923914a394c6aa5273f1b07f82e
coming1024 <57028337+coming1024@users.noreply.github.com>
2024-04-20 16:33:29 -0400

    Delete myproject directory

--- myproject/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserRegisterView, UserLoginView
-from myapp import views
-urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name='register'),
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('', views.home, name='home'),
-]
